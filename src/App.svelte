d72df80f1bdfdcea1775d2979363f33c1de04414
Wilson Andres Naranjo Romero <wanaranjo@gmail.com>
2020-11-03 01:12:54 -0500

    update components

--- src/App.svelte
+++ src/App.svelte
@@ -17,10 +17,10 @@
 <main class="flex items-center py-1 justify-center md:h-screen bg-gray-100">
   <div class="flex flex-col md:flex-row container mx-auto text-gray-800">
     <div
-      class="w-full md:w-2/5 border-b-4 border-orange-600 md:border-r-4 md:border-b-0 px-5 md:px-0">
+      class="w-full md:w-1/2 border-b-4 border-orange-600 md:border-r-4 md:border-b-0 px-5 md:px-0">
       <h1 class="text-5xl leading-tight">
         Hola! Soy
-        <strong class="font-bold">Wilson Naranjo</strong>
+        <strong class="font-bold">Wilson Andrés Naranjo Romero</strong>
       </h1>
       <div class="text-orange-600 mt-3 text-md">
         <div class="py-3">
@@ -33,7 +33,7 @@
             <p class="px-3">wanaranjor</p>
           </a>
         </div>
-        <div class="flex items-center py-1 py-3">
+        <div class="flex items-center py-3">
           <a
             href="https://github.com/wanaranjor"
             class="flex items-center py-1"
@@ -43,7 +43,7 @@
             <p class="px-3">wanaranjor</p>
           </a>
         </div>
-        <div class="flex items-center py-1 py-3">
+        <div class="flex items-center py-3">
           <a
             href="https://www.linkedin.com/in/wanaranjor/"
             class="flex items-center py-1"
@@ -53,7 +53,7 @@
             <p class="px-3">wanaranjor</p>
           </a>
         </div>
-        <div class="flex items-center py-1 py-3">
+        <div class="flex items-center py-3">
           <a
             href="mailto:contacto@wanaranjor.co"
             class="flex items-center py-1"
@@ -66,9 +66,9 @@
       </div>
     </div>
     <div
-      class="w-full md:w-3/5 flex flex-col justify-center items-center py-3 md:py-0">
+      class="w-full md:w-1/2 flex flex-col justify-center items-center py-3 md:py-0">
       <p class="w-full px-3 text-xl">
-        Desarrollor Frontend en constante formación.
+        Desarrollador Frontend en constante formación.
       </p>
       <ul class="w-full px-10 py-5">
         <li class="flex items-center py-1">
